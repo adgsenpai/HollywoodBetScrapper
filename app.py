a22c0154371e0c59097fda174effee57e8e7f3e6
adgsenpai <adg@adgstudios.co.za>
2022-10-23 02:36:19 +0200

    First Commit

--- app.py
+++ app.py
@@ -5,4 +5,18 @@ from selenium.webdriver.common.keys import Keys
 broswer = webdriver.Chrome(
     executable_path='./chromedriver_win32/chromedriver.exe')
 
-broswer.get('https://adgstudios.co.za/')
+broswer.get('https://m.hollywoodbets.net/Menu/Betting/TodaySportCoupon.aspx?sn=Soccer&s=1&bti=15')
+
+# get the table ctl00_MainContent_popularCouponTable
+table = broswer.find_element(By.ID, 'ctl00_MainContent_popularCouponTable')
+
+# get the rows markets-row
+rows = table.find_elements(By.CLASS_NAME, 'markets-row')
+
+buffer = []
+
+for row in rows:
+    buffer.append(row.text)
+
+for item in buffer:
+    print(item)
